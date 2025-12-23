3f11fd91f009f638e6e7866dde73997a0dba6317
mdbakibillahrohan <mdbakibillah2022@gmail.com>
2025-12-23 21:19:23 +0600

    fixed problems of category

--- src/modules/master/master.router.ts
+++ src/modules/master/master.router.ts
@@ -4,10 +4,10 @@ import { Router } from "express";
 // Controllers
 import { createCategoryController, createCategorySchema } from "./controllers/category/createCategory.controller.js";
 import { getCategoryListController, getCategoryListSchema } from "./controllers/category/getCategoryList.controller.js";
-import { createCollectionController, createCollectionSchema } from "./controllers/category/createCollection.controller.js";
-import { getCollectionListSchema, getCollectionListController } from "./controllers/category/getCollectionList.controller.js";
-import { createVendorController, updateVendorController, vendorSchemas } from "./controllers/category/createVendor.controller.js";
-import { getVendorListController } from "./controllers/category/getVendorList.controller.js";
+import { createCollectionController, createCollectionSchema } from "./controllers/collection/createCollection.controller.js";
+import { getCollectionListSchema, getCollectionListController } from "./controllers/collection/getCollectionList.controller.js";
+import { createVendorController, updateVendorController, vendorSchemas } from "./controllers/vendor/createVendor.controller.js";
+import { getVendorListController } from "./controllers/vendor/getVendorList.controller.js";
 import authMiddleware from "../../middlewares/auth.middleware.js";
 import requestValidatorMiddleware from "../../middlewares/request-validator.middleware.js";
 
--- src/router.ts
+++ src/router.ts
@@ -6,13 +6,6 @@ import masterRouter from "./modules/master/master.router.js";
 const appRouter = Router();
 
 appRouter.use("/auth", authRouter);
-<<<<<<< HEAD
  appRouter.use("/user",userRouter)
 appRouter.use("/master", masterRouter);
 export default appRouter;
-=======
-appRouter.use("/user", userRouter);
-appRouter.use("/master", masterRouter);
-
-export default appRouter;
->>>>>>> main
