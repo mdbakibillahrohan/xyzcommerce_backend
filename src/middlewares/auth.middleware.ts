9eec92c57f5b6bc7f4f6c7fa2428fa111807da1b
mdbakibillahrohan <mdbakibillah2022@gmail.com>
2026-01-17 19:58:34 +0600

    static file serving problem solved

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,18 +2,28 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import configuration from "../config/config.js";
 
-const authMiddleware = (req:Request, res:Response, next:NextFunction)=>{
-    const accessToken = req.headers["authorization"]?.split(" ")[1];
-    if(accessToken){
-        const userData = jwt.verify(accessToken, configuration.TOKEN_SECRET);
-        req.user = userData;
-        next();
-        return;
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const accessToken = req.headers["authorization"]?.split(" ")[1];
+  try {
+    if (accessToken) {
+      const userData = jwt.verify(accessToken, configuration.TOKEN_SECRET);
+      req.user = userData;
+      next();
+      return;
     }
 
-    return res.json({
-        message:"Unauthorized"
-    }).status(401);
-}
+    return res
+    .status(401)
+      .json({
+        message: "Unauthorized",
+      });
+  } catch (err:any) {
+    console.log(err?.message)
+    return res.status(401)
+      .json({
+        message: err?.message || "Unauthorized",
+      });
+  }
+};
 
 export default authMiddleware;
